ace922a3be0f4867d6f44d79edc584586baa7297
KatharinaGitStar <168305372+KatharinaGitStar@users.noreply.github.com>
2024-06-03 18:15:50 +0200

    changes after testing MyEventDateComparator.java

--- src/MusicLandscape/util/comparators/MyEventDateComparator.java
+++ src/MusicLandscape/util/comparators/MyEventDateComparator.java
@@ -1,6 +1,8 @@
 package MusicLandscape.util.comparators;
 
 import MusicLandscape.entities.Event;
+import java.util.Date;
+import java.util.Calendar;
 
 public class MyEventDateComparator extends MyEventComparator {
 
@@ -10,29 +12,39 @@ public class MyEventDateComparator extends MyEventComparator {
         //than any non-null event.
         if (e1 == null && e2 == null) {
             return 0; // Both are null, they are equal
-        }
-        else if (e1 == null && e2 != null) {
+        } else if (e1 == null) {
             return -1; // e1 is null and e2 is not null, e1 is smaller
-        }
-        else if (e2 == null && e1 != null) {
+        } else if (e2 == null) {
             return 1; // e2 is null and e1 is not null, e2 is smaller
         }
         // Both e1 and e2 are not null, so compare their dates
-        else if (e1.getDate() == null && e2.getDate() == null) {
+        if (e1.getDate() == null && e2.getDate() == null) {
             return 0; // Both dates are null
-        }
-        else if (e1.getDate() == null) {
+        } else if (e1.getDate() == null) {
             return -1; // e1's date is null and e2's date is not null, e1's date is considered smaller
-        }
-        else if (e2.getDate() == null) {
+        } else if (e2.getDate() == null) {
             return 1; // e2's date is null and e1's date is not null, e2's date is considered smaller
-        }
-        else if(e1.getDate().equals(e2.getDate())){
-            return 1;
-        }
-        else
-        {   // Use compareTo to compare the dates
-            return e1.getDate().compareTo(e2.getDate());
+        } else {   // Use compareTo() to compare the dates
+            // Use compareTo() to compare the dates
+            Date date1 = e1.getDate();
+            Date date2 = e2.getDate();
+            Date normalizedDate1 = normalizeDate(date1);
+            Date normalizedDate2 = normalizeDate(date2);
+            int result = normalizedDate1.compareTo(normalizedDate2);
+            return result;
         }
     }
+
+
+    public Date normalizeDate(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime();
+    }
 }
+
+
